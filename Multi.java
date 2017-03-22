49424be8c70c437fb4d918863fa2cbe7b91f648a
Swarthon <swarthon.gokan@gmail.com>
2017-03-22 12:14:46 +0100

    Changelog : 	- Code cleaning aka "Magic Touch" 	- Minor changes on the output

--- Multi.java
+++ Multi.java
@@ -5,43 +5,37 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+
 class Multi {
 	public static void main(String[] args)
 						throws FileNotFoundException {
-		Scanner keyboard=new Scanner (System.in);
+		Scanner keyboard = new Scanner (System.in);
 		String yes = "y";
-		int ndte, ndm, a, mp, mg;
-		ndm = 0;
-		ndte = 0;
-		a = 0;
-		mp = 0;
-		mg = 0;
-		while (ndm < 10 && ndte < 3){
-			ndte =0;
-			Random r = new Random();
-			int b = r.nextInt(10)+1;
-			int p = r.nextInt(10)+1;
-			ndm=ndm+1;
-			while (a!=b*p){
-				System.out.print(b);
+		int failed = 0, played = 0, input = 0, lose = 0, win = 0;
+		while (played < 10 && failed < 3){
+			failed = 0;
+			Random rand = new Random();
+			int nb1 = rand.nextInt(10)+1;
+			int nb2 = rand.nextInt(10)+1;
+			played++;
+			while ( input != nb1 * nb2){
+				System.out.print(nb1);
 				System.out.print("x");
-				System.out.print(p);
+				System.out.print(nb2);
 				System.out.println();
-				a=keyboard.nextInt(); 
-				if (a==b*p){
-					mg = mg+1;
+				input = keyboard.nextInt();
+				if (input == nb1 * nb2){
+					win++;
 					System.out.println("Correct");
-					System.out.println("============================================================================");
-					if (ndm==10){
+					System.out.println("=======\n");
+					if (played == 10)
 						System.out.println("Results");
-					}
 				}
-				if (a!=b*p){
-					ndte=ndte+1;
-					if (ndte == 1){
+				if (input != nb1 * nb2) {
+					failed++;
+					if (failed == 1)
 						System.out.println("False, try again");
-					}
-					if (ndte == 2){
+					if (failed == 2) {
 						System.out.println("False, Next Round");
 						break;
 					}
@@ -49,26 +43,25 @@ class Multi {
 			}
 		}
 		System.out.print("Won Rounds : ");
-		System.out.print(mg);
+		System.out.print(win);
 		System.out.print("\nLost Rounds : ");
-		System.out.print(mp);
+		System.out.print(lose);
 		System.out.print("\n Save Score? [y/n]");
-		Scanner keyb=new Scanner (System.in);
-		String z=keyb.nextLine();
-		if (yes.equals(z)){
+		Scanner keyb = new Scanner (System.in);		// TODO avoid creation of a second keyboard
+		String ans = keyb.nextLine();
+		if (yes.equals(ans)) {
 			try(FileWriter fw = new FileWriter("Scores.txt", true);
-				    BufferedWriter bw = new BufferedWriter(fw);
-				    PrintWriter out = new PrintWriter(bw))
+				BufferedWriter	bw	= new BufferedWriter(fw);
+				PrintWriter	out	= new PrintWriter(bw))
 				{
-				    out.print("Won Rounds : ");
-				    out.print(mg);
-					out.print("			Lost Rounds : ");
-					out.print(mp);
+					out.print("Won Rounds : ");
+					out.print(win);
+					out.print("\t\t\tLost Rounds : ");
+					out.print(lose);
 					out.print("\n");
-				} catch (IOException e) {
+				} catch (IOException e) {}
+			keyb.close();
+			keyboard.close();
 		}
-		keyb.close();
-		keyboard.close();
 	}
-}
 }
